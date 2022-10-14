e2c384da3938861ed48677fac3ad0922b4c352ca
sabrina <sabrinaabrodi20@gmail.com>
2022-10-14 19:18:09 +0200

    feat: saving to local storage

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 const notesContainer = document.getElementById("notes");
 const addNoteButton = notesContainer.querySelector(".add-note");
+
+function getNotes() {
+  return JSON.parse(localStorage.getItem("stickynotes-notes") || "[]");
+}
+
+function saveNote(notes) {
+  localStorage.setItem("stickynotes-notes", JSON.stringify(notes));
+}
