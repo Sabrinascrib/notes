88103e20ff287df1609617bb6eb2c70b4f80f05c
sabrina <sabrinaabrodi20@gmail.com>
2022-10-15 20:49:37 +0200

    feat: adding create element function

--- script.js
+++ script.js
@@ -8,3 +8,27 @@ function getNotes() {
 function saveNote(notes) {
   localStorage.setItem("stickynotes-notes", JSON.stringify(notes));
 }
+
+function createNoteElement(id, content) {
+  const element = document.createElement("textarea");
+
+  element.classList.add("note");
+  element.value = content;
+  element.placeholder = "Empty Sticky Note";
+
+  element.addEventListener("change", () => {
+    updateNote(id, element.value);
+  });
+
+  element.addEventListener("dblclick", () => {
+    const doDelete = confirm(
+      "Are you sure you wish to delete this sticky note?"
+    );
+
+    if (doDelete) {
+      deleteNote(id, element);
+    }
+  });
+
+  return element;
+}
